a7f593a98bb93ec18491590452cf497e07fed2b1
WilsonWang881 <Wilson.Wang@WilsonWang.org>
2020-05-14 21:09:00 -0500

    first commit

---
+++ precache-manifest.8f8bf2213c8fec02ca35391714191f1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549b792d314c315d24a75a091539fff2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5a46a4c8c0c98f93c44",
+    "url": "./static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "077bf30735688b7ae6ad",
+    "url": "./static/css/main.3daae77d.chunk.css"
+  },
+  {
+    "revision": "a5a46a4c8c0c98f93c44",
+    "url": "./static/js/2.0cd2b905.chunk.js"
+  },
+  {
+    "revision": "9e899dfd7ae2e062da19e83984708bc5",
+    "url": "./static/js/2.0cd2b905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077bf30735688b7ae6ad",
+    "url": "./static/js/main.426c0076.chunk.js"
+  },
+  {
+    "revision": "41a3578c8a137fa34916",
+    "url": "./static/js/runtime-main.01fed7e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
